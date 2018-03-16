ce52b58b224a34c6bf3219d04006730e6565c9ee
Gustavus Shaw II <gshaw1997@gmail.com>
2018-03-16 11:00:55 -0400

    Remove axios replace with built in request

--- src/drivers/json-store.ts
+++ src/drivers/json-store.ts
@@ -1,27 +1,30 @@
-import { DataStore } from "../interfaces/DataStore";
-const axios = require('axios');
-// import * as axios from "axios"; Why doesn't this work?
+import { DataStore } from '../interfaces/DataStore';
+import * as request from 'request';
 
 export class JsonStore implements DataStore {
   getUsers() {
-    return axios.get(`http://localhost:3000/users`)
-      .then(res => res.data);
+    return request.get(`http://localhost:3000/users`).then(res => res.data);
   }
   getUser(id: string) {
-    return axios.get(`http://localhost:3000/users/${id}`)
+    return request
+      .get(`http://localhost:3000/users/${id}`)
       .then(res => res.data);
   }
   addUser(name: string) {
-    return axios.post('http://localhost:3000/users', {
-      name
-    }).then(res => res.data);
+    return request
+      .post('http://localhost:3000/users', {
+        name
+      })
+      .then(res => res.data);
   }
   updateUser(id: string, name: string) {
-    return axios.patch('http://localhost:3000/users/' + id, { id, name })
+    return request
+      .patch('http://localhost:3000/users/' + id, { id, name })
       .then(res => res.data);
   }
   deleteUser(id: string) {
-    return axios.delete('http://localhost:3000/users/' + id)
+    return request
+      .delete('http://localhost:3000/users/' + id)
       .then(res => res.data);
   }
 }
